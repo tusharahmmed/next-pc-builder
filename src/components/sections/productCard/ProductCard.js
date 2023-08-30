2a5f9456e38f8673c5de97d5113775431956d3e4
Tushar Ahmmed Sakib <tusharahmmedsakib@gmail.com>
2023-08-30 13:54:55 +0600

    [done] homepage design

---
+++ src/components/sections/productCard/ProductCard.js
@@ -0,0 +1,26 @@
+import styles from "@/styles/componensts/productCard.module.css";
+import Image from "next/image";
+
+const ProductCard = () => {
+  return (
+    <div className={styles.item}>
+      <div className={styles.img}>
+        <Image
+          alt="name"
+          width={200}
+          height={200}
+          layout="responsive"
+          src={"/images/products/monitor/1.webp"}
+        />
+      </div>
+      <div className={styles.details}>
+        <h4>
+          MSI G2412 23.8 FHD 170Hz IPS 1ms FreeSync Premium Gaming Monitor
+        </h4>
+        <p>23,800৳</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,61 @@
 import RootLayout from "@/layouts/RootLayout";
+import Image from "next/image";
+import styles from "@/styles/index.module.css";
+import CategoryCard from "@/components/sections/categoryCard/CategoryCard";
+import ProductCard from "@/components/sections/productCard/ProductCard";
 
 export default function Homepage() {
   return (
-    <div className="h-screen">
-      <h1 className="text-4xl">This is home page vr</h1>
+    <div className="spacing-x py-8 bg-[#f2f4f8;]">
+      <div className={`${styles.heroWraper}`}>
+        <div className="">
+          <Image
+            src={"/images/hero/02-laptop-960x450.png"}
+            height={500}
+            width={600}
+            alt=""
+            layout="responsive"
+          />
+        </div>
+        <div className="flex flex-col gap-[20px]  justify-between">
+          <div>
+            <Image
+              src={"/images/hero/side1.png"}
+              height={500}
+              width={600}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+          <div>
+            <Image
+              src={"/images/hero/side2.png"}
+              height={600}
+              width={600}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="py-20">
+        <div className="text-center">
+          <h2 className="title">Featured Products</h2>
+          <p className="subtitle">Check & Get Your Desired Product!</p>
+        </div>
+        <div className={styles.productWraper}>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </div>
     </div>
   );
 }
